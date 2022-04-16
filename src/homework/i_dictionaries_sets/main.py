7b5ca7d51a4027447476a4ea8ece118d701d04b5
R3dShadow <arianna.adamson@g.austincc.edu>
2022-04-15 21:59:03 -0500

    Homework 6 finished

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,10 @@
-#
+import dictionary
+
+list1 = input('\nEnter a DNA string: ')
+list2 = input('\nEnter a DNA string: ')
+list3 = input('\nEnter a DNA string: ')
+list4 = input('\nEnter a DNA string: ')
+
+result = dictionary.get_p_distance_matrix(list1, list2, list3, list4)
+
+print('Here is your matrix: ' + str(result))
