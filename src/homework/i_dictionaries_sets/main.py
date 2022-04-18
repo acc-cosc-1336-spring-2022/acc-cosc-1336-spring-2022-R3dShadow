0946f905cd2f59d58783d2aa5e159f451292dbee
R3dShadow <arianna.adamson@g.austincc.edu>
2022-04-18 12:51:39 -0500

    Homework 7 done

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,10 +1,33 @@
 import dictionary
 
-list1 = input('\nEnter a DNA string: ')
-list2 = input('\nEnter a DNA string: ')
-list3 = input('\nEnter a DNA string: ')
-list4 = input('\nEnter a DNA string: ')
+def menu():
+    choice = input('\nMenu\n\n1-Add or update item\n2-Remove item\n3-Exit\n\nChoose an option: ')
+    if choice == '1':
+        add_item()
+    elif choice == '2':
+        del_item()
+    else:
+        print('\nExiting..\n')
 
-result = dictionary.get_p_distance_matrix(list1, list2, list3, list4)
+def add_item():
+    print('\nCURRENT INVENTORY\n' + str(dictionary.inventory))
+    key = input('\nEnter an item name: ')
+    quantity = int(input('\nEnter inventory changes: '))
+    print('\nNEW INVENTORY\n' + str(dictionary.add_inventory(key, quantity)))
+    choice = input('\nWould you like to continue?\n\n1-Menu\n2-Exit\n\nChoose an option: ')
+    if choice == '1':
+        menu()
+    else:
+        print('\nExiting..\n')
 
-print('Here is your matrix: ' + str(result))
+def del_item():
+    print('\nCURRENT INVENTORY\n' + str(dictionary.inventory))
+    key = input('\nEnter an item name: ')
+    print('\nNEW INVENTORY\n' + str(dictionary.remove_inventory(key)))
+    choice = input('\nWould you like to continue?\n\n1-Menu\n2-Exit\n\nChoose an option: ')
+    if choice == '1':
+        menu()
+    else:
+        print('\nExiting..\n')
+
+menu()
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,8 +1,12 @@
+from ast import Add
 import unittest
 
-from src.homework.i_dictionaries_sets.dictionary import get_p_distance
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory
 
 class Test_Config(unittest.TestCase):
 
-    def test_distance(self):
-        self.assertEqual(get_p_distance('TTTCCATTTA', 'TTACCTATTA'), .3)
+    def test_inventory(self):
+        self.assertEqual(add_inventory('balls', 10), {'balls': 10})
+
+    def test_remove_invetory(self):
+        self.assertEqual(remove_inventory('B'), {})
