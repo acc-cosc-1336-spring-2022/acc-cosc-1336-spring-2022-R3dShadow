dad4e0332558035c48a22e31d4df47c2510f89e7
R3dShadow <arianna.adamson@g.austincc.edu>
2022-05-05 20:53:56 -0500

    Homework 9 Complete

--- src/homework/j_classes/class_a.py
+++ src/homework/j_classes/class_a.py
@@ -1,1 +1,28 @@
-#
+import random
+
+class Die:
+
+    def roll(self):
+        __roll_value = random.randint(1, 6)
+        return __roll_value
+
+    def get_roll(self):
+        return self.roll()
+    
+    def __str__(self):
+        return str("\nThe rolled value is {0}.\n".format(self.get_roll()))
+
+def main():
+    die = Die()
+    decision = input("\nWould you like to roll the Die? Y or N.\n\nEnter answer here: ")
+    while decision == 'Y':
+        print(die)
+        decision = input("Would you like to roll the Die again? Y or N.\n\nEnter answer here: ")
+    while decision == 'N':
+        print("\nGame is ending..\n")
+        quit()
+    else:
+        print('\nInvalid input. Enter Y or N.\n')
+        main()
+
+main()
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -1,1 +1,38 @@
-#
+import unittest
+from src.homework.j_classes import class_a
+
+class Test_Config(unittest.TestCase):
+    
+    def test_dice_roll_1(self):
+        die = class_a.Die()
+        die.roll()
+        if die.get_roll_value() in [1, 2, 3, 4, 5, 6]:
+            RealRoll = True
+        else:
+            RealRoll = False
+        self.assertEqual(True, RealRoll)
+    
+    def test_dice_roll_2(self):
+        my_die = class_a.Die()
+        my_die.roll()
+        if my_die.get_roll_value() in [1, 2, 3, 4, 5, 6]:
+            RealRoll = True
+        else:
+            RealRoll = False
+        self.assertEqual(True, RealRoll)
+
+    def test_dice_roll_3(self):
+        the_die = class_a.Die()
+        the_die.roll()
+        if the_die.get_roll_value() in [1, 2, 3, 4, 5, 6]:
+            RealRoll = False
+        self.assertEqual(True, RealRoll)
+
+    def test_dice_roll_fail(self):
+        a_die = class_a.Die()
+        a_die.roll()
+        if a_die.get_roll_value() in [7, 8, 9, 10, 11, 12]:
+            RealRoll = True
+        else:
+            RealRoll = False
+        self.assertEqual(False, RealRoll)
