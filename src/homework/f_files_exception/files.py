77ec8f29d5986fa759f8e6a71ee20481e95c35f4
R3dShadow <arianna.adamson@g.austincc.edu>
2022-05-05 19:19:36 -0500

    Homework 8 Finished

--- src/homework/f_files_exception/files.py
+++ src/homework/f_files_exception/files.py
@@ -1,1 +1,19 @@
-#
+inventory = {'Pizza': 15}
+
+def add_inventory(name, quantity):
+    if name not in inventory:
+        inventory[name] = quantity
+        invFile = open('inventory.txt', 'w')
+        invFile.write(str(inventory) + '\n')
+        invFile.close()
+        print('Done 1')
+    else:
+        inventory[name] += quantity
+        invFile = open('inventory.txt', 'w')
+        invFile.write(str(inventory) + '\n')
+        invFile.close()
+        print('Done 2')
+    return inventory
+
+def display_inventory(file):
+    file = open('inventory.txt', 'r')
--- src/homework/f_files_exception/main.py
+++ src/homework/f_files_exception/main.py
@@ -1,1 +1,31 @@
-#
+import files
+
+def menu():
+    choice = input('\nMenu\n\n1-Add or update item\n2-Display items\n3-Exit\n\nChoose an option: ')
+    if choice == '1':
+        add_item()
+    elif choice == '2':
+        del_item()
+    else:
+        print('\nExiting..\n')
+
+def add_item():
+    print('\nCURRENT INVENTORY\n' + str(files.inventory))
+    key = input('\nEnter an item name: ')
+    quantity = int(input('\nEnter inventory changes: '))
+    print('\nNEW INVENTORY\n' + str(files.add_inventory(key, quantity)))
+    choice = input('\nWould you like to continue?\n\n1-Menu\n2-Exit\n\nChoose an option: ')
+    if choice == '1':
+        menu()
+    else:
+        print('\nExiting..\n')
+
+def del_item():
+    print('\nCURRENT INVENTORY\n' + str(files.inventory))
+    choice = input('\nWould you like to continue?\n\n1-Menu\n2-Exit\n\nChoose an option: ')
+    if choice == '1':
+        menu()
+    else:
+        print('\nExiting..\n')
+
+menu()
